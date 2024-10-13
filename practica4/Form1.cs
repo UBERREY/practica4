0a5c13c0f96f401b21ad609c9fb2263c5efdcc01
UBERREY <ubereyescamacho@hotmail.com>
2024-10-12 19:31:32 -0600

    Version 3 de formulario

--- practica4/Form1.cs
+++ practica4/Form1.cs
@@ -1,3 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+
 namespace practica4
 {
     public partial class Form1 : Form
@@ -5,15 +20,49 @@ namespace practica4
         public Form1()
         {
             InitializeComponent();
+            tbNombres.TextChanged += validarNombre;
+            tbApellidos.TextChanged += validarApellido;
+            tbTelefono.TextChanged += validarTelefono;
+            tbEdad.TextChanged += validarEdad;
+            tbEstatura.TextChanged += validarEstatura;
         }
+
+        string DBConection = "Server=localhost;Port=3306;Database=programacionavanzada;Uid=root;Pwd=Migato_105;";
+
+
+        void insertRegistry(string nombres, string apellidos, int edad, float estatura, long celular, string genero)
+        {
+            using (MySqlConnection connection = new MySqlConnection(DBConection))
+            {
+                connection.Open();
+
+                string insertQuery = "insert into registros (nombres, apellidos, edad, estatura, celular, genero) " +
+                    "values (@nombres, @apellidos, @edad, @estatura, @celular, @genero)";
+
+                using (MySqlCommand command = new MySqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@nombres", nombres);
+                    command.Parameters.AddWithValue("@apellidos", apellidos);
+                    command.Parameters.AddWithValue("@celular", celular);
+                    command.Parameters.AddWithValue("@estatura", estatura);
+                    command.Parameters.AddWithValue("@edad", edad);
+                    command.Parameters.AddWithValue("@genero", genero);
+
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             String nombres = tbNombres.Text;
             String apellidos = tbApellidos.Text;
-            String telefono = tbTelefono.Text;
-            String edad = tbEdad.Text;
-            String estatura = tbEstatura.Text;
+            long celular = long.Parse(tbTelefono.Text);
+            int edad = int.Parse(tbEdad.Text);
+            float estatura = float.Parse(tbEstatura.Text);
             String genero = "";
 
             if (rbMujer.Checked)
@@ -25,7 +74,7 @@ namespace practica4
                 genero = rbHombre.Text;
             }
 
-            String datos = $"nombres : {nombres} \r\napellidos: {apellidos}\r\nedad: {edad}\r\nestatura: {estatura}\r\ntelefono: {telefono}\r\ngenero: {genero}\r\n";
+            String datos = $"nombres : {nombres} \r\napellidos: {apellidos}\r\nedad: {edad}\r\nestatura: {estatura}\r\ntelefono: {celular}\r\ngenero: {genero}\r\n";
 
             String rutadearchivo = "C:\\Users\\lenovo\\source\\repos\\practica4\\datos.txt";
 
@@ -33,6 +82,8 @@ namespace practica4
             {
                 writer.WriteLine(datos);
             }
+
+            insertRegistry(nombres, apellidos, edad, estatura, celular, genero);
 
             MessageBox.Show("Datos Guardados con Exito");
         }
@@ -45,5 +96,102 @@ namespace practica4
             tbEdad.Clear();
             tbEstatura.Clear();
         }
+        private bool EsTextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, @"^[a-zA-Z\s]+$");
+        }
+        private void validarNombre(object sender, EventArgs e)
+        {
+            if (!EsTextoValido(tbNombres.Text))
+            {
+                MessageBox.Show("ingrese un nombre valido", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbNombres.Clear();
+            }
+        }
+
+        private void validarApellido(object senter, EventArgs e)
+        {
+            if (!EsTextoValido(tbApellidos.Text))
+            {
+                MessageBox.Show("Ingrese un apellido valido", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbApellidos.Clear();
+            }
+        }
+
+
+
+        private bool EsEnteroValido(string valor)
+        {
+            int resultado;
+            return int.TryParse(valor, out resultado);
+        }
+        private void validarEdad(object senter, EventArgs e)
+        {
+            if (!EsEnteroValido(tbEdad.Text))
+            {
+                MessageBox.Show("ingrese una edad valida", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbEdad.Clear();
+            }
+        }
+
+
+        private void validarEstatura(object senter, EventArgs e)
+        {
+            if (!EsDecimalValido(tbEstatura.Text))
+            {
+                MessageBox.Show("Ingrese una estatura valida (en metros)", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbEstatura.Clear();
+            }
+        }
+        private bool EsDecimalValido(string valor)
+        {
+            float resultado;
+            return float.TryParse(valor, out resultado);
+        }
+
+        private void validarTelefono(object sender, EventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;
+            string input = textbox.Text;
+            if (textbox.Text.Length == 10 && EsEnteroValidoDe10Digitos(textbox.Text))
+            {
+                textbox.BackColor = Color.Green;
+            }
+            else
+            {
+                textbox.BackColor = Color.Red;
+            }
+
+        }
+        private bool EsEnteroValidoDe10Digitos(string valor)
+        {
+            long resultado;
+            return long.TryParse(valor, out resultado) && valor.Length == 10;
+        }
+        private void tbNombres_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbApellidos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbTelefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbEdad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbEstatura_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
